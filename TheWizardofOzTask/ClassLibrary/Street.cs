ac9be311949a715cc2693aa14c7f13713c4b545d
Shair <akhkhdilavar@edu.hse.ru>
2020-05-19 18:38:53 +0300

    Task1 without the serialization part

---
+++ TheWizardofOzTask/ClassLibrary/Street.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    [Serializable]
+    public class Street
+    {
+        public string Name { get; private set; }
+        private int[] _houses;
+
+        public Street(string name, int[] houseNumbers)
+        {
+            if (houseNumbers == null || name == null)
+                throw new ArgumentNullException();
+            Name = name;
+            _houses = houseNumbers;
+        }
+
+        private int _numberOfHouses => _houses.Length;
+        public static int operator ~(Street street) => street._numberOfHouses;
+        public static bool operator !(Street street)
+        {
+            var a = (from h in street._houses
+                     where h.ToString().Contains('7')
+                     select h).ToList();
+            return a.Count != 0;
+        }
+
+        public override string ToString()
+        {
+            if (_houses == null)
+                throw new ArgumentNullException("no street initiated");
+            var sb = new StringBuilder();
+            sb.Append(Name + Environment.NewLine);
+            for (int i = 0; i < _houses.Length - 1; i++)
+                sb.Append(_houses[i] + ", ");
+            sb.Append(_houses[_houses.Length - 1]);
+            return sb.ToString();
+        }
+    }
+}
---
+++ TheWizardofOzTask/ConsoleApp/Program1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    class Program1
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            char sep = Path.DirectorySeparatorChar;
+            string path = $"data.txt";
+            int N = GetInt();
+            List<Street> streets = new List<Street>();
+            if (CheckStreetDataFile(path))
+            {
+                var streetsData = File.ReadAllLines(path);
+
+                foreach (var street in streetsData)
+                {
+                    string[] str = street.Split(new char[] { ' ' });
+                    int temp = 0;
+                    var nums = (from t in str
+                                where int.TryParse(t, out temp)
+                                select temp).ToList();
+                    try
+                    {
+
+                    streets.Add(new Street(str[0], nums.ToArray()));
+                    }
+                    catch (ArgumentNullException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        return;
+                    }
+                }
+            }
+            else
+                FillStreetData(streets, N);
+            if (streets.Count < N)
+                FillStreetData(streets, N - streets.Count);
+
+            foreach (var street in streets)
+                Console.WriteLine(street);
+        }
+
+        private static void FillStreetData(List<Street> streets, int n)
+        {
+            for (int i = 0; i < n; i++)
+                streets.Add(new Street(GenerateName(), GenerateIntArr()));
+        }
+
+        static string GenerateName()
+        {
+            int len = rnd.Next(3, 8);
+            var sb = new StringBuilder();
+            sb.Append((char)rnd.Next('A', 'Z' + 1));
+            for (int i = 0; i < len; i++)
+                sb.Append((char)rnd.Next('a', 'z' + 1));
+
+            return sb.ToString();
+        }
+
+        static int[] GenerateIntArr()
+        {
+            var nums = new List<int>();
+            int len = rnd.Next(1, 11);
+
+            for (int i = 0; i < len; i++)
+                nums.Add(rnd.Next(1, 101));
+
+            return nums.ToArray();
+        }
+
+        static int GetInt()
+        {
+            int n = 0;
+            while (!int.TryParse(Console.ReadLine(), out n) || n < 1 || n > 1000)
+            {
+                Console.Clear();
+                Console.WriteLine("Incorret input");
+            }
+            Console.Clear();
+            Console.WriteLine(n + " streets");
+            return n;
+        }
+
+        static bool CheckStreetDataFile(string path)
+        {
+            if (!File.Exists(path))
+                return false;
+
+            var streetsData = File.ReadAllLines(path);
+
+            if (streetsData.Length == 0)
+                return false;
+            bool validData = true;
+
+            foreach (var street in streetsData)
+            {
+                string[] str = street.Split(new char[] { ' ' });
+                if (str.Length < 2)
+                    return false;
+                int temp = 0;
+                var nums = (from t in str
+                            where int.TryParse(t, out temp)
+                            select t).ToList();
+
+                if (nums.Count != str.Length - 1)
+                {
+                    validData = false;
+                    break;
+                }
+            }
+
+            return validData;
+        }
+    }
+
+}
